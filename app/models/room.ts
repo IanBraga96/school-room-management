0197994c5cecd5933b97a1c260bd4439df12fab9
Ian Braga <contatoianbraga@gmail.com>
2024-07-27 18:50:01 -0300

    Criando models user, room e allocation

---
+++ app/models/room.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
+import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+import User from './user.js'
+
+export default class Room extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare room_number: number
+
+  @column()
+  declare capacity: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @manyToMany(() => User, {
+    pivotTable: 'allocations',
+  })
+  declare users: ManyToMany<typeof User>
+}
--- app/models/user.ts
+++ app/models/user.ts
@@ -1,9 +1,11 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+import Room from './room.js'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -15,7 +17,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare id: number
 
   @column()
-  declare fullName: string | null
+  declare name: string | null
 
   @column()
   declare email: string
@@ -23,11 +25,31 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column({ serializeAs: null })
   declare password: string
 
+  @column()
+  declare registration_number: string
+
+  @column.dateTime()
+  declare birthDate: DateTime | null
+
+  @column()
+  declare isAdmin: boolean
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
 
-  static accessTokens = DbAccessTokensProvider.forModel(User)
+  @manyToMany(() => Room, {
+    pivotTable: 'allocations',
+  })
+  declare rooms: ManyToMany<typeof Room>
+
+  static accessTokens = DbAccessTokensProvider.forModel(User, {
+    expiresIn: '30 days',
+    prefix: 'oat_',
+    table: 'auth_access_tokens',
+    type: 'auth_token',
+    tokenSecretLength: 40,
+  })
 }
